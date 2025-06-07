4238c6890cd360bc6bb176186cf8f50b4221f884
Abhishek779-Star <abhishekbro779@gmail.com>
2025-06-07 21:31:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -84,11 +84,11 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/Howtoverifyanylink') 
 # ============================
 # Link Shortener Configuration
 # ============================
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', false))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Linkshortify.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '7cc1e69ba5d053e9124dcd34bf56f94b66cb6f23')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Howtoverifyanylink')  # Tutorial video link for opening shortlink website
-IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
+IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', false))
 
 # ============================
 # Channel & Group Links Configuration
