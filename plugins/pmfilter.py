7e9932197b9441714f98d117620c3934b4e746d9
Abhishek779-Star <abhishekbro779@gmail.com>
2025-06-07 08:56:37 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2139,7 +2139,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         
     elif query.data == "source":
         buttons = [[
-            InlineKeyboardButton('Sᴏᴜʀᴄᴇ Cᴏᴅᴇ 📜', url='https://t.me/AV_King1'),
+            InlineKeyboardButton('💌 ᴄᴏɴᴛᴀᴄᴛ', url='https://t.me/AV_King1'),
             InlineKeyboardButton('⇋ ʙᴀᴄᴋ ⇋', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
